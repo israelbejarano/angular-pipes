662d69f9968cc266cd7257e65b948b9657895237
israel <israel.bejarano.a@gmail.com>
2019-01-19 13:09:30 +0100

    code up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+  nombre = 'Israel';
+  nombre2 = 'isRAel bEjarAnO ÁlVaREZ';
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: '19'
+    }
+  };
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Llego el dato'), 3500);
+  });
+  fecha = new Date();
+  video = '-2VPKWwl5Rw';
 }
